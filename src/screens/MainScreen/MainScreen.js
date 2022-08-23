7630b1d5ff12377d101715467e6f5df797049174
Artem Kazaryan <artem.kazaryan@saltpepper.ru>
2022-08-23 15:44:35 +0300

    configure webview

--- src/screens/MainScreen/MainScreen.js
+++ src/screens/MainScreen/MainScreen.js
@@ -1,15 +1,57 @@
-import {StyleSheet} from 'react-native';
-import React from 'react';
+import {
+  handleMessagesFromReactNativeOnWebPage,
+  sendMessageFromWebPageToReactNative,
+} from '@/utils/webviewExamples';
+import React, {useCallback, useRef} from 'react';
+import {Button, StyleSheet} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import WebView from 'react-native-webview';
 
+const runFirst = `
+  window.isNativeApp = true;
+  true; // note: this is required, or you'll sometimes get silent failures
+`;
+
+const html = `
+  <html>
+    <head></head>
+    <body>
+    <script>
+      ${handleMessagesFromReactNativeOnWebPage}
+      ${sendMessageFromWebPageToReactNative}
+    </script>
+    <h1>React Native Webview</h1>
+    </body>
+  </html>
+`;
+
 const MainScreen = () => {
+  const webviewRef = useRef(null);
+
+  const sendMessageToWebPage = useCallback(() => {
+    webviewRef.current.postMessage('Hello');
+    console.log('send');
+  }, []);
+
+  const webviewMessageHandler = useCallback(
+    event => {
+      console.log('Get from web page:', event.nativeEvent.data);
+      sendMessageToWebPage();
+    },
+    [sendMessageToWebPage],
+  );
+
   return (
     <SafeAreaView style={styles.root}>
       <WebView
+        ref={webviewRef}
         style={styles.webview}
-        source={{uri: 'https://www.google.com/'}}
+        source={{html}}
+        injectedJavaScriptBeforeContentLoaded={runFirst}
+        onMessage={webviewMessageHandler}
+        javaScriptEnabled={true}
       />
+      <Button title="Send message" onPress={sendMessageToWebPage} />
     </SafeAreaView>
   );
 };
@@ -18,9 +60,7 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
   },
-  webview: {
-    flex: 1,
-  },
+  webview: {flex: 1},
 });
 
 export default MainScreen;
