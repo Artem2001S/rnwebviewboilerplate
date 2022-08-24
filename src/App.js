91d6d93e64667ed14269af79820da48437f62695
Artem Kazaryan <artem.kazaryan@saltpepper.ru>
2022-08-24 14:43:20 +0300

    add and configure one signal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import useOneSignal from './hooks/useOneSignal';
 
 import RootStackNavigator from '@/navigation/RootStackNavigator';
 
 const App = () => {
+  useOneSignal({notificationOpenAppHandler: () => {}});
+
   return (
     <NavigationContainer>
       <RootStackNavigator />
--- src/config/index.js
+++ src/config/index.js
@@ -3,4 +3,5 @@ import env from 'react-native-config';
 export const config = {
   webviewUrl: env.WEBVIEW_URL,
   isProduction: env.IS_PRODUCTION === '1',
+  ONESIGNAL_APP_ID: env.ONESIGNAL_APP_ID,
 };
